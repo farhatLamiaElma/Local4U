8a1568651f168738e2099951ea1684056bb4cdd7
Adavadkar <aditi.davadkar78@gmail.com>
2024-11-28 09:53:15 -0500

    Final Chat Bot feature

--- static/js/chatscript.js
+++ static/js/chatscript.js
@@ -1,9 +1,14 @@
-
-    (function(d, m){
-        var kommunicateSettings =
-            {"appId":"31029a20502d700acd5c35fb251e9c4c1","popupWidget":true,"automaticChatOpenOnNavigation":true};
-        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-        window.kommunicate = m; m._globals = kommunicateSettings;
-    })(document, window.kommunicate || {});
+<script type="text/javascript">
+    (function (d, m
+){
+    var kommunicateSettings =
+{"appId":"261d9139039ca3c4bd129c1c6a6802b7b","popupWidget":true,"automaticChatOpenOnNavigation":true};
+    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+    s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+    var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+    window.kommunicate = m; m._globals = kommunicateSettings;
+})
+(document, window.kommunicate || {})
+;
+    /* NOTE : Use web server to view HTML files as real - time update will not work if you directly open the HTML file in the browser. */
+</script>
